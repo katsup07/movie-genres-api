4976387be8c9fb925ad36fdaee85cbe9d70ed34a
katsup <brett_7y7@yahoo.ca>
2022-08-16 16:51:47 +0900

    Update .gitignore

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ require('./startup/config')();
 require('./startup/logging')();
 
 require('./startup/routes')(app);
+require('./startup/prod')(app);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
